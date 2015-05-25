fef23746675fcc723d57c7ef086d857b2cf08504
Anton Dietrich <korenchikov19@gmail.com>
2015-05-26 00:01:19 +0300

    Settings for Release mode

--- build.bat
+++ build.bat
@@ -17,8 +17,10 @@ if not exist "bin" md bin
 goto COMMON
 
 :RELEASE
-rem goto COMMON
-goto NOCANDO
+set compilerFlags=-Ox -Oi -EHsc -GR- -MT -W4 -nologo -D_DEBUG_
+rem set suppressedWarnings=-wd4530
+set output=-Fdbin\ -Fobuild\ -Febin\blocks.exe
+goto COMMON
 
 :COMMON
 set source=src\main.cpp src\game.cpp src\renderer.cpp
